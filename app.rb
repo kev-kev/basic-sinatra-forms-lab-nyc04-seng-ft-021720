281882120659af728109f28df47c8186f26f28d0
kev-kev <kevin.clark93@gmail.com>
2020-03-09 20:55:25 -0400

    Done.

--- app.rb
+++ app.rb
@@ -2,5 +2,25 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+  get('/newteam'){
+    erb :newteam
+  }
+  post('/newteam'){
+    erb :team
+  }
 
+  get ('/team'){
+    erb :team
+  }
+  
+  post('/team'){
+    @name=params[:name]
+    @coach=params[:coach]
+    @pg=params[:pg]
+    @sg=params[:sg]
+    @pf=params[:pf]
+    @sf=params[:sf]
+    @c=params[:c]
+    erb :team
+  }
 end
